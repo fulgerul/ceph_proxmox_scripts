6f2035783acb4462e730f9487b79d1a7b3a187e1
Fulgerul <me@me.com>
2018-09-01 14:12:41 +0200

    Work on swapoff

--- swapoff.sh
+++ swapoff.sh
@@ -1,6 +1,7 @@
 #!/bin/sh -e
-#
-# rc.local
+
+
+# /etc/rc.local
 #
 # This script is executed at the end of each multiuser runlevel.
 # Make sure that the script will "exit 0" on success or any other
@@ -10,11 +11,31 @@
 # bits.
 #
 # By default this script does nothing.
-# Dont forget: chmod +x /etc/rc.local
-# swapoff -a
 
 # fix for proxmox kernel swappiness problem, yes, it must be executed twice
 for i in `/usr/bin/find /sys /var/lib 2>/dev/null | /bin/grep slice | /bin/grep swappiness` ; do /bin/echo 0 >"${i}" ; done
 for i in `/usr/bin/find /sys /var/lib 2>/dev/null | /bin/grep slice | /bin/grep swappiness` ; do /bin/echo 0 >"${i}" ; done
 
 exit 0
+
+# Dont forget: chmod +x /etc/rc.local
+# swapoff -a
+
+# cat /sys/fs/cgroup/memory/system.slice/pvedaemon.service/memory.swappiness
+# should be 0
+
+# free -m
+# Should say Swap used: 0
+
+# grep "Hugepagesize:" /proc/meminfo
+# Should say 2048 or 4096
+
+
+# In /etc/sysctl.conf
+# vm.swappiness=0
+# vm.vfs_cache_pressure=50
+# vm.dirty_background_ratio = 5
+# vm.dirty_ratio = 10
+# vm.min_free_kbytes=2097152
+# vm.zone_reclaim_mode=1
+# vm.nr_hugepages = 4300
