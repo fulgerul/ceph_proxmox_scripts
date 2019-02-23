e2b112b0244cf4ec8ed5971e6ca838740591885d
Fulgerul <me@me.com>
2019-02-23 19:59:08 +0100

    Mellanox firmware upgrade on Proxmox

--- change_subnet.sh
+++ change_subnet.sh
@@ -1,12 +1,21 @@
 #https://forum.proxmox.com/threads/changing-ceph-public-network.33083/
 ## NOT TESTED YET, ONLY A STUB!
-cd /etc/pve/proxmox_custom_conf
-monmaptool  --create  --add  pve11 172.16.1.11:6789 --add pve12 172.16.1.12:6789 --add pve13 172.16.1.13:6789 monmap | monmaptool --print monmap
-then stop the monitors
-# and reload the monmap into the ceph configuration (on all monitors - be sure to match the right mon-id):
-# i = is the mon-id
-ceph-mon -i 2 --inject-monmap monmap
 
+# While the mon is running on the old IP
+ceph mon getmap -o monmap.txt && monmaptool --print monmap.txt
+
+# Remove current monitor from the monmap file
+monmaptool --rm $HOSTNAME monmap.txt
+
+# Add monitor with new ip to the monmap file
+monmaptool  --add  $HOSTNAME 10.0.0.x:6789  monmap.txt && monmaptool --print monmap.txt
+
+systemctl stop ceph-mon@*
+
+# Inject this new monmap into the hurrent monitor
+ceph-mon -i $HOSTNAME --inject-monmap monmap.txt
+
+reboot
 
 
 # change the configs
